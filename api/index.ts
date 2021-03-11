30661a602e0d885ebbf7d03dfde6e6445d5e0535
Tony Ketcham <tonyketcham@gmail.com>
2021-03-11 14:41:32 -0600

    provide example api route

--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,8 @@ import { getHtml } from './_lib/template';
 const isDev = !process.env.AWS_REGION;
 const isHtmlDebug = process.env.OG_HTML_DEBUG === '1';
 
+// example API route: http://localhost:3000/2020%20Snoozefest.jpg?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fpuerh.wtf%2Fassets%2Fstatic%2Fapple-touch-icon.7b22250.d6c38f098e4cfe7492d30929042211dd.png&images=https%3A%2F%2Fres.cloudinary.com%2Fpu-erh%2Fimage%2Fupload%2Fv1612895806%2Ftea%2F2021%2FSnoozefest%2F63B60F99-5C28-4C00-9818-633EBA3A259D_iytjc2.jpg&widths=undefined&widths=500&heights=undefined&heights=500
+
 export default async function handler(req: IncomingMessage, res: ServerResponse) {
     try {
         const parsedReq = parseRequest(req);
